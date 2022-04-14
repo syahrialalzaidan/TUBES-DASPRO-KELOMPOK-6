950919eab856790cc1619dfe2fe023f70a2f4981
azkadhika3 <42485978+azkadhika3@users.noreply.github.com>
2022-04-14 21:31:23 +0700

    Add files via upload

---
+++ fungsi.py
@@ -0,0 +1,48 @@
+def inputdata(file, data):
+# Kalau ada yang punya ide buat input data di line baru boleh ubah aja
+# File: tempat filenya (string cth:"user.csv"), data : isi data yang mau dimasukin (string udah dipisah pake ';')
+# data yang diinput berupa teks yang sudah dipisah dengan koma
+    folder = open(file, "a")
+    folder.write("\n" + data)
+    folder.close()
+
+
+def caridata(file,urutan,cek):
+    # Ini kalau misalnya datanya engga ketemu bakal ngeluarin teks yang engga ketemu
+    # File: tempat filenya (string cth:"user.csv")
+    # Urutan : integer (nah kan nanti tuh si teks per line dari filenya di split terus teksnya berubah jadi matriks,
+    #                   si urutan ini tuh nandain indeks data yang dicari di bagian mana, dimulai dari 0)
+    # cek : string (fungsi ini tuh awalnya buat nyari data yang elemennya sama kek password sama username, jadi ada ini
+    #               kalau misalnya banyak fungsi yang engga perlu pake variabel ini bilang aja)
+    # Keluarannya list
+    folder = open(file, "r")
+    for a in folder.readlines():
+        b = split(a)
+        if b[urutan] == cek:
+            return b
+        else:
+            continue
+    return "tidak ketemu bang"
+
+
+def split(a):   # Note kalau misalnya nginput datanya pake fungsi input data si '\n' kebawa di elemen list terakhir
+    # a : string yang dipisah ';'
+    # keluaran list
+    total = 0
+    for i in a:
+        if i == ";":
+            total += 1
+        else:
+            continue
+    hasil = ["" for i in range(total+1)]
+    teks = ""
+    k = 0
+    for j in a:
+        if j == ";":
+            hasil[k] = teks
+            k += 1
+            teks = ""
+        else:
+            teks += j
+    hasil[k] = teks
+    return hasil
