3ba4ab63e380a4e6ecc1aa938be9ab67f322c88a
samuel-eric <30772879+samuel-eric@users.noreply.github.com>
2022-04-21 15:01:05 +0700

    Update F15.py

--- F15.py
+++ F15.py
@@ -17,16 +17,19 @@ def load():
   import os.path
   from os import path 
 
+  folder = False
   # Validasi folder
   if args.nama_folder is None: # jika pengguna tidak memasukkan nama folder
     print("Tidak ada nama folder yang diberikan!")
     print("Usage: python program_binomo.py <nama_folder>")
+    return folder
   else: # jika pengguna memasukkan nama folder, akan dicek apakah folder ada atau tidak
     if path.isdir(args.nama_folder): # jika folder ada 
       print("Loading...")
       print('Selamat datang di antarmuka "Binomo"')
+      os.chdir("./{}".format(args.nama_folder)) # Pindah folder
+      folder = True
+      return folder
     else: # jika nama folder yang diberikan pengguna tidak ada
       print("Folder {} tidak ditemukan.".format(args.nama_folder))
-  
-  # Pindah folder
-  os.chdir("./{}".format(args.nama_folder))
+      return folder
