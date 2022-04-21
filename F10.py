bd1a3b802095646c30fa0b18698af036682c2007
syahrialalzaidan <88835497+syahrialalzaidan@users.noreply.github.com>
2022-04-21 13:31:47 +0700

    Update F10.py
    revisi

--- F10.py
+++ F10.py
@@ -1,115 +1,61 @@
-from fungsi import split
+from fungsi import csv_to_array, split, count_csv_column,count_csv_row, samecount
+username = input('Masukkan Username: ') #kalo belom diinput dan kalo udah ada berarti harus ganti variabel di fungsinya
 
-def output_f10(a, tahun, nomor) :
-    print(str(nomor) + '.', end=' ') #ketemu
-    nomor += 1
+def search_my_game() :
+    inventory = csv_to_array('kepemilikan.csv')
+    data_game = csv_to_array('game.csv')
+    nomor = 1
+    #nyari game yang dimiliki 
+    my_game = []
+    count = 0 
+    found = False
+    for y in my_game :
+        count += 1
+    for x in inventory :
+        if username == x[1] :
+            my_game += [x[0]]
 
-    for spek in a :
-        if spek != tahun and spek != a[5] : 
-            print(spek, end=' | ')
-        elif spek == tahun :
-            print(spek)
-        else :
-            pass
-    
-    
-
-def search_my_game() : #data diambil dari file game.csv yang udah di sort berdasarkan id
     try :
-        id = input('Masukkan ID Game: ')
+        id = input('Masukan ID Game: ')
         tahun = input('Masukkan Tahun Rilis Game: ')
-        data = open('game.csv','r')
-        nomor = 1
-        found = False
-        if id != '' and tahun != '' : 
-            print('Daftar game pada inventory yang memenuhi kriteria: ')
-            for x in data.readlines() :
-                a = split(x)
-                #ngatur ulang list biar sesuai yang diminta abangnya
-                kategori = a[2]
-                year = a[3]
-                harga = a[4]
-                a[2] = harga 
-                a[3] = kategori
-                a[4] = year
+        if id != '' and tahun != '' :
+            if id in my_game :
+                for x in data_game :
+                    if x[0] == id and x[3] == tahun :
+                        found = True
+                        print(f'{nomor}. {(x[0])}   | {(x[1])} | {(x[4])}    | {(x[2])} | {(x[3])}')
+                        nomor += 1
 
-                if a[0] == id and a[4] == tahun :
-                    found = True
-                    output_f10(a,tahun, nomor)
-                else :
-                    continue
             if not found :
                 print('Tidak ada game pada inventory-mu yang memenuhi kriteria')
-        
-
     except :
         pass
     else :
         if id == '' and tahun != '' :
-            nomor = 1
-            found = False
-            print('Daftar game pada inventory yang memenuhi kriteria: ')
-            for x in data.readlines() :
-                a = split(x)
-                #ngatur ulang list biar sesuai yang diminta abangnya
-                kategori = a[2]
-                year = a[3]
-                harga = a[4]
-                a[2] = harga 
-                a[3] = kategori
-                a[4] = year
-
-                if a[4] == tahun :
+            for x in data_game :
+                if x[3] == tahun :
                     found = True
-                    output_f10(a,tahun, nomor)    
+                    print(f'{nomor}. {(x[0])}   | {(x[1])} | {(x[4])}    | {(x[2])} | {(x[3])}')
                     nomor += 1
-                else :
-                    continue
-            if not found :
-    
-                print('Tidak ada game pada inventory-mu yang memenuhi kriteria')
-        
-        elif tahun == '' and id != '': 
-            nomor = 1
-            found = False
-            print('Daftar game pada inventory yang memenuhi kriteria: ')
-            for x in data.readlines() :
-                a = split(x)
-                #ngatur ulang list biar sesuai yang diminta
-                kategori = a[2]
-                year = a[3]
-                harga = a[4]
-                a[2] = harga 
-                a[3] = kategori
-                a[4] = year
-
-                if a[0] == id :
-                    found = True
-                    output_f10(a,tahun, nomor)
-                    nomor += 1
-        
-                else :
-                    continue
             if not found :
                 print('Tidak ada game pada inventory-mu yang memenuhi kriteria')
-
-        elif tahun == '' and id == '' :
-            nomor = 1
-            print('Daftar game pada inventory yang memenuhi kriteria: ')
-            for x in data.readlines() :
-                a = split(x)
-                #ngatur ulang list biar sesuai yang diminta abangnya
-                if a[0] != 'id' : 
-                    kategori = a[2]
-                    year = a[3]
-                    harga = a[4]
-                    a[2] = harga 
-                    a[3] = kategori
-                    a[4] = year
-
-                    output_f10(a ,year, nomor) 
-                    nomor += 1   
+            
+        elif id != '' and tahun == '' :
+            if id in my_game :
+                for x in data_game :
+                    if x[0] == id :
+                        found = True
+                        print(f'{nomor}. {(x[0])}   | {(x[1])} | {(x[4])}    | {(x[2])} | {(x[3])}')
+                        nomor += 1
+            if not found :
+                print('Tidak ada game pada inventory-mu yang memenuhi kriteria')
+                    
+        
+        elif id == '' and tahun == '' :
+            for x in data_game :
+                if x[0] in my_game :
+                    print(f'{nomor}. {(x[0])}   | {(x[1])} | {(x[4])}    | {(x[2])} | {(x[3])}')
+                    nomor += 1
         else :
             pass
-
 search_my_game()
