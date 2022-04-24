8e47e41c11bea107de6c78a5bcedd2e3e05ec47d
azkadhika3 <42485978+azkadhika3@users.noreply.github.com>
2022-04-24 17:37:48 +0700

    Nambah komen

--- F05.py
+++ F05.py
@@ -1,5 +1,7 @@
 import fungsi
 def cekkosong(nama):
+    # Menginput sebuah string lalu mengeluarkan False jika string tidak kosong dan True jika string kosong 
+    # i : char
     for i in nama:
         if i != " " and i != "":
             return False
@@ -9,6 +11,24 @@ def cekkosong(nama):
 
 
 def ubahGame(data):
+    # Fungsi mengubah data dari id game yang dipilihnya
+    # I.S. : data tidak kosong
+	# F.S. : Procedur menginput data data yang diperlukan untuk menambah sebuah game dan divalidasi agar datanya tidak ada yang kosong, 
+    #        kemudian prosedur mengeluarkan data yang baru  
+
+    
+    # Kamus lokal
+    # id : string
+    # datagame : array [0..5] of string
+    # nama : string
+    # kategori : string
+    # tahunrilis : string
+    # harga : string
+    # stok : string
+    # databaru : array [0..5] of string
+    # i : integer
+    # indeks : integer
+    
     # Menginput ID game yang ingin diubah
     id = input("Masukan ID game: ")
 
@@ -18,7 +38,7 @@ def ubahGame(data):
     # Mencek isi dari data tersebut
     if not datagame:
         print("ID game tidak ditemukan")
-        return
+        return data
 
     # Mengisi data baru untuk gamenya
     else:
