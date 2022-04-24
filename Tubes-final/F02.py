8844c497caeeae1ee6cdd9a9bb943f4ba5fcba65
azkadhika3 <42485978+azkadhika3@users.noreply.github.com>
2022-04-24 17:43:50 +0700

    Nambah komen

--- Tubes-final/F02.py
+++ Tubes-final/F02.py
@@ -1,6 +1,9 @@
 import fungsi
 import B01
 def cekusername(username):
+    # Fungsi mencek apakah username yang diinput valid atau tidak
+    # Kamus lokal
+    # i : char
     for i in username:
         if ord(i) == 45 or ord(i) == 95 or 65 <= ord(i) <= 90 or 97 <= ord(i) <= 122 or 48 <= ord(i) <= 57:
             continue
@@ -10,6 +13,14 @@ def cekusername(username):
 
 
 def register(data):
+    # I.S. : data tidak kosong
+ 	# F.S. : procedur menginput data data untuk dimasukan dalam userbaru yang kemudian jika sudah divalidasi akan dimasukan dalam data 
+
+    # Kamus lokal
+    # nama : string
+    # username : string
+    # password : string
+    # userbaru : array [0..5] of string
     # Input nama
     nama = input("Masukan nama: ")
 
@@ -35,3 +46,4 @@ def register(data):
         return data
     else:
         print("Username " + username + " sudah ada. Silahkan gunakan username lain!")
+        return data
