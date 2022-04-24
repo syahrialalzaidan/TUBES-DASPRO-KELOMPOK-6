fec40ff6201fd8f3a54d43e20c725eacbe0db37c
azkadhika3 <42485978+azkadhika3@users.noreply.github.com>
2022-04-24 17:24:01 +0700

    Nambah komentar

--- F02.py
+++ F02.py
@@ -1,6 +1,9 @@
 import fungsi
 import B01
 def cekusername(username):
+    # Menginput sebuah string lalu mengeluarkan False jika string tidak kosong dan True jika string kosong
+    # Kamus lokal
+    # i : char
     for i in username:
         if ord(i) == 45 or ord(i) == 95 or 65 <= ord(i) <= 90 or 97 <= ord(i) <= 122 or 48 <= ord(i) <= 57:
             return True
@@ -9,6 +12,15 @@ def cekusername(username):
 
 
 def register(data):
+    # Fungsi merigister user baru dimasukan ke data
+    # I.S. : data tidak kosong
+ 	# F.S. : procedur menginput data data untuk dimasukan dalam userbaru yang kemudian jika sudah divalidasi akan dimasukan dalam data
+    # Kamus
+    # nama : string
+    # username : string
+    # password : string
+    # id : integer
+    # userbaru : array [0..5] of string
     # Input nama
     nama = input("Masukan nama: ")
 
@@ -31,6 +43,7 @@ def register(data):
 
     # Mencek apakah usernamenya sudah ada atau tidak
     if fungsi.caridatalist(data, 1, username) == []:
+        print("Username " + username + " berhasil ditambahkan!")
         return data
     else:
         print("Username " + username + " sudah ada. Silahkan gunakan username lain!")
